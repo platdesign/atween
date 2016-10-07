4027b0d5491a396a78944d718bd8d91905fd5ed6
Christian Blaschke <mail@platdesign.de>
2016-10-07 15:20:07 +0200

    added some tests

--- index.js
+++ index.js
@@ -4,8 +4,56 @@
 module.exports = class Atween {
 
 	constructor() {
+		this._hooks = {};
+	}
+
+	inputInterceptor() {
 
 	}
+
+	outputInterceptor() {
+
+	}
+
+	register(name) {
+		let stack = this._getOrCreateHookStack(name);
+	}
+
+	run(name) {
+		let stack = this._getHookStack(name);
+	}
+
+
+
+
+
+
+	_getOrCreateHookStack(name) {
+
+		let stack;
+
+		try {
+			stack = this._getHookStack(name);
+		} catch(e) {
+			stack = [];
+		}
+
+		this._hooks[name] = stack;
+
+		return stack;
+	}
+
+	_getHookStack(name) {
+
+		let stack = this._hooks[name];
+
+		if(!stack) {
+			throw new Error('Hook-Stack not found');
+		}
+
+		return stack;
+	}
+
 
 
 }
--- test/index.js
+++ test/index.js
@@ -9,12 +9,53 @@ const Atween = require('../');
 
 describe('atween', function() {
 
-	it('should create instance', () => {
+	describe('instance', () => {
 
-		let instance = new Atween();
+		let instance;
 
-		expect(instance).to.be.an.object();
+		beforeEach(() => instance = new Atween());
+
+		it('should be an object', () => expect(instance).to.be.an.object());
+		it('should be an instance of Atween', () => expect(instance).to.be.instanceOf(Atween));
+		it('should have method: inputInterceptor', () => expect(instance.inputInterceptor).to.be.a.function());
+		it('should have method: register', () => expect(instance.register).to.be.a.function());
+		it('should have method: outputInterceptor', () => expect(instance.outputInterceptor).to.be.a.function());
+		it('should have method: run', () => expect(instance.run).to.be.a.function());
+
+		it('should have private hooks-store', () => expect(instance._hooks).to.be.an.object());
+		it('should have private method: _getOrCreateHookStack', () => expect(instance._getOrCreateHookStack).to.be.a.function());
+
+		it('_getOrCreateHookStack() should return new stack', () => {
+
+			let stack = instance._getOrCreateHookStack('test');
+
+			expect(stack)
+				.to.be.an.array()
+				.have.length(0);
+
+		});
+
+
+		it('should have private method: _getHookStack', () => expect(instance._getHookStack).to.be.a.function());
+
+		it('_getHookStack() should throw error on unknown stack', () => {
+
+			expect(() => instance._getHookStack('test'))
+				.to.throw(Error, 'Hook-Stack not found')
+
+		});
+
+		it('_getHookStack() should return expected stack', () => {
+
+			instance._hooks['test'] = [1, 2, 3];
+
+			expect(instance._getHookStack('test'))
+				.to.be.an.array()
+				.to.have.length(3)
+				.to.equal([1,2,3]);
+
+		});
 
 	});
 
-})
+});
